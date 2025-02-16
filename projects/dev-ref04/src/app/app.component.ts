88a8aeee829cc675d773928cf9ced0c66cdad9d0
k.osawa <saataysu4408999@gmail.com>
2025-02-16 15:46:01 +0900

    modified

--- projects/dev-ref04/src/app/app.component.ts
+++ projects/dev-ref04/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from "../../../dev-ref02/src/app/parent/parent.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ projects/dev-ref04/src/app/child/child.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss'
+})
+export class ChildComponent {
+  @Input() buttonText: string = 'BUTTON';
+  @Input() buttonColor: string = '';
+
+
+}
